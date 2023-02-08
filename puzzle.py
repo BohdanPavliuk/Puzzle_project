c298ee4379504369858e761d049f18d70b77785b
Vspotiv <bohdanpavlyk122@gmail.com>
2023-02-08 09:04:09 +0200

    Main file for updating

---
+++ puzzle.py
@@ -0,0 +1,15 @@
+def main_check(array):
+    pass
+
+def row_check(row):
+    pass
+
+def column_check(column):
+    pass
+
+def colours_check(row):
+    pass
+
+
+def validate_board(board):
+    pass
